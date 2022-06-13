396721678d9164c1f0b99ee9685a2e87d6c56eaa
Daniel Horta <skanciado@gmail.com>
2022-06-13 15:06:40 +0200

    first commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
