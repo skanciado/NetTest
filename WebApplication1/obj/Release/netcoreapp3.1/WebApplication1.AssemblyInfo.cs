fdaa9be11966b2ba06894b407b0ad6b7fd9b1b4d
Daniel Horta <skanciado@gmail.com>
2022-06-22 15:59:39 +0200

    DockerFile

--- WebApplication1/obj/Release/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("80a0f6b0-07f4-48a2-8a87-db7ddbe67d38")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
